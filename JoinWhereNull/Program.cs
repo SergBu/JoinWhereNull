0a8655c8b31597e9bfb0f2dac2d8863d72a5b85e
Сергей Будников < @LAPTOP-H6G5MQOH>
2023-12-04 14:04:40 +0300

    Добавьте файлы проекта.

---
+++ JoinWhereNull/Program.cs
@@ -0,0 +1,15 @@
+﻿var yearStarted = DateTime.UtcNow.AddYears(-1);
+
+var iDsWhereStateExists = await GetIdsAsync(context);
+
+var terminalTimeslotVehicles = await (from v in context.TerminalTimeslotVehicles.AsNoTracking()
+                                      where v.DateCreated > yearStarted
+                                      join s in context.TerminalTimeslotVehicleStates.AsNoTracking()
+                                           on v.Id equals s.TerminalTimeslotVehicleId into vs
+                                      from s in vs.DefaultIfEmpty()
+                                      where s == null
+                                      orderby v.Id
+                                      select v
+                                )
+                                .Take(10000)
+                                .ToListAsync();
